9abe8ecdc3f237e5fe10b39b53502be62b1a1b32
jimfarrugia <the.jim.farrugia@gmail.com>
2023-02-14 16:44:47 +1100

    add colors and gradients to constants

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const colors = {
+  black: "#15171b",
+  grayDark: "#21222c",
+  gray: "#414558",
+  white: "#f8f8f2",
+  blue: "#6272a4",
+  cyan: "#8be9fd",
+  green: "#50fa7b",
+  orange: "#ffb86c",
+  pink: "#ff79c6",
+  purple: "#bd93f9",
+  red: "#ff5555",
+  yellow: "#f1fa8c",
+  pinkPurple: `linear-gradient(135deg, #ff79c6, #bd93f9)`,
+  purpleCyan: `linear-gradient(135deg, #bd93f9, #8be9fd)`,
+  yellowPink: `linear-gradient(135deg, #f1fa8c, #ff79c6)`,
+  cyanGreen: `linear-gradient(135deg, #8be9fd, #50fa7b)`,
+};
