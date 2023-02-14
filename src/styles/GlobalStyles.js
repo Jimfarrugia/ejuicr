9c2b51b232344beae0f0a09428c09d0be5377693
jimfarrugia <the.jim.farrugia@gmail.com>
2023-02-14 20:30:38 +1100

    fix indentation

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,13 +2,13 @@ import { createGlobalStyle } from "styled-components";
 import { colors } from "../constants";
 
 const GlobalStyles = createGlobalStyle`
-    body {
-        margin: 0;
-        padding: 0;
-        color: ${colors.white};
-        background: ${colors.grayDark};
-        font-family: sans-serif;
-    }
+  body {
+    margin: 0;
+    padding: 0;
+    color: ${colors.white};
+    background: ${colors.grayDark};
+    font-family: sans-serif;
+  }
 `;
 
 export default GlobalStyles;
