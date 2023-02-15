cc8d9a314bc8b7a6b637f5ebe62853e8acb7d270
jimfarrugia <the.jim.farrugia@gmail.com>
2023-02-15 22:09:26 +1100

    migrate from CRA to Vite with Nx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,15 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
-import Layout from "./components/Layout";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // <Link /> elements will fail to render unless they are a within a Router component
-// const layout = (
-//   <Router>
-//     <Layout />
-//   </Router>
-// );
 const header = (
   <Router>
     <Header />
@@ -20,13 +13,6 @@ test("renders the header logo", () => {
   render(header);
   const logo = screen.getByAltText("ejuicr logo");
   expect(logo).toBeTruthy();
-});
-
-test("renders hamburger menu icon", () => {
-  // render(layout);
-  // const menuIcon = screen.findByTestId("app-footer");
-  // expect(Footer).toBeTruthy();
-  throw new Error();
 });
 
 // header nav should be rendered and contain each link
