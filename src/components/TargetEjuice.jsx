763c3e087917c072bf9437443d9bafbe46060e75
jimfarrugia <the.jim.farrugia@gmail.com>
2023-02-17 18:32:03 +1100

    control TargetEjuice form values with state

--- src/components/TargetEjuice.jsx
+++ src/components/TargetEjuice.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import NumberControls from "./NumberControls";
 
 const TargetEjuice = () => {
+  const [targetPg, setTargetPg] = useState(30);
+  const [targetVg, setTargetVg] = useState(70);
+  const [targetNicStrength, setTargetNicStrength] = useState(6);
+  const [targetAmount, setTargetAmount] = useState(150);
+
+  const onChangeTargetPg = (e) => setTargetPg(e.target.value);
+  const onChangeTargetVg = (e) => setTargetVg(e.target.value);
+  const onChangeTargetNicStrength = (e) => setTargetNicStrength(e.target.value);
+  const onChangeTargetAmount = (e) => setTargetAmount(e.target.value);
+
   return (
     <>
       <h3>Target Ejuice</h3>
@@ -10,11 +21,11 @@ const TargetEjuice = () => {
         <div>
           <span className="label-left">PG/VG</span>
           <div className="input-border">
-            <input type="number" value="30" />
+            <input type="number" value={targetPg} onChange={onChangeTargetPg} />
           </div>
           <span className="label-between">/</span>
           <div className="input-border">
-            <input type="number" value="70" />
+            <input type="number" value={targetVg} onChange={onChangeTargetVg} />
           </div>
         </div>
         <NumberControls />
@@ -24,7 +35,11 @@ const TargetEjuice = () => {
         <h5>Strength:</h5>
         <div>
           <div className="input-border">
-            <input type="number" value="6" />
+            <input
+              type="number"
+              value={targetNicStrength}
+              onChange={onChangeTargetNicStrength}
+            />
           </div>
           <span className="label-right">mg/mL</span>
         </div>
@@ -35,7 +50,12 @@ const TargetEjuice = () => {
         <h5>Amount:</h5>
         <div>
           <div className="input-border">
-            <input type="number" className="wide" value="150" />
+            <input
+              type="number"
+              className="wide"
+              value={targetAmount}
+              onChange={onChangeTargetAmount}
+            />
           </div>
           <span className="label-right">mL</span>
         </div>
