df6eba53945bee0f06fb7a684634e7371a010e09
jimfarrugia <the.jim.farrugia@gmail.com>
2023-02-17 19:28:16 +1100

    add increment/decrement functions to NumberControls

--- src/components/NumberControls.jsx
+++ src/components/NumberControls.jsx
@@ -3,13 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 import { NumberControlsStyled } from "./styled/NumberControls.styled";
 
-const NumberControls = () => {
+const NumberControls = ({ value, handler, step }) => {
+  const increment = () => handler(value + step);
+  const decrement = () => handler(value - step);
+
   return (
     <NumberControlsStyled>
-      <button type="button">
+      <button type="button" onClick={decrement}>
         <FontAwesomeIcon icon={faMinus} />
       </button>
-      <button type="button">
+      <button type="button" onClick={increment}>
         <FontAwesomeIcon icon={faPlus} />
       </button>
     </NumberControlsStyled>
