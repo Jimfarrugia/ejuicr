70af6247086c4cf42cd9961988f06cbb2c451309
jimfarrugia <the.jim.farrugia@gmail.com>
2023-02-18 02:02:14 +1100

    refactor NumberControls and round values

--- src/components/NumberControls.jsx
+++ src/components/NumberControls.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { roundToTwoDecimalPlaces } from "../helpers";
 import { NumberControlsStyled } from "./styled/NumberControls.styled";
 
 const NumberControls = ({ value, handler, step, min }) => {
-  const increment = () => handler(value + step);
+  const increment = () => handler(roundToTwoDecimalPlaces(+value + step));
   const decrement = () => {
-    // If the min prop is not nullish and the result would be < min, return min
-    if (min !== (undefined || null) && value - step < min) {
+    if (min !== (undefined || null) && +value - step < min) {
       return handler(min);
     }
-    handler(value - step);
+    handler(roundToTwoDecimalPlaces(+value - step));
   };
 
   return (
