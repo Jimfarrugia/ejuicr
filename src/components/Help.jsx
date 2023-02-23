31854f18e2ea94e679c1aba6ed69e1cff21d5492
jimfarrugia <the.jim.farrugia@gmail.com>
2023-02-24 04:54:10 +1100

    Add help page content

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Help = () => {
-  return <div>Help</div>;
+  return (
+    <>
+      <h3>Help</h3>
+      <hr />
+      <h4>Contact Support</h4>
+      <hr />
+      <p>
+        If you’re having trouble with ejuicr and would like to reach out you can
+        send an email to{" "}
+        <a href="mailto:help+ejuicr@gmail.com">help+ejuicr@gmail.com</a> and
+        someone will get back to you.
+      </p>
+      <p>
+        You can also send a tweet or a direct message on Twitter to{" "}
+        <a href="https://twitter.com/ejuicr" target="_blank">
+          @ejuicr
+        </a>{" "}
+        but you may have to wait a bit longer for a reply.
+      </p>
+      <h4>Send Feedback</h4>
+      <hr />
+      <p>
+        We appreciate any feedback you’d like to share! Please email{" "}
+        <a href="mailto:feedback+ejuicr@gmail.com">feedback+ejuicr@gmail.com</a>{" "}
+        or tweet{" "}
+        <a href="https://twitter.com/ejuicr" target="_blank">
+          @ejuicr
+        </a>
+        .
+      </p>
+    </>
+  );
 };
 
 export default Help;
