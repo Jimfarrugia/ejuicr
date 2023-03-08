ab19b2e83b5ddabc891d507bc1df7a888377c6c6
jimfarrugia <the.jim.farrugia@gmail.com>
2023-03-08 22:51:31 +1100

    refresh page if "/" link is clicked from "/" route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,17 @@ import { HeaderStyled, MenuButton } from "./styled/Header.styled";
 import logoImg from "../assets/logo.svg";
 
 const Header = ({ toggleMenu }) => {
+  const handleClick = () => {
+    // force page refresh if logo is clicked from "/" route
+    if (window.location.pathname === "/") {
+      window.location.reload();
+    }
+  };
+
   return (
     <HeaderStyled>
       <div className="container">
-        <Link to="/">
+        <Link to="/" onClick={handleClick}>
           <img src={logoImg} alt="ejuicr logo" />
         </Link>
         <MenuButton type="button" title="menu" onClick={toggleMenu}>
