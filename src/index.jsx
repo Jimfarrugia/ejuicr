d2a823ba715506a6c10f71e5c873f3d3cec3cc4c
jimfarrugia <the.jim.farrugia@gmail.com>
2023-03-15 02:05:34 +1100

    use ThemeProvider and create dark theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import "typeface-bebas-neue";
 import App from "./App";
 import Layout from "./components/Layout";
@@ -10,26 +11,34 @@ import Settings from "./components/Settings";
 import About from "./components/About";
 import Help from "./components/Help";
 import Contribute from "./components/Contribute";
-import GlobalStyles from "./styles/GlobalStyles";
 import UpdatePassword from "./components/UpdatePassword";
+import GlobalStyles from "./styles/GlobalStyles";
+import { light, dark } from "./styles/themes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyles />
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<App />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/recipes/:id" element={<Recipe />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/contribute" element={<Contribute />} />
-          <Route path="/update-password/:token" element={<UpdatePassword />} />
-        </Routes>
-      </Layout>
+      <ThemeProvider
+        theme={localStorage.getItem("theme") === "light" ? light : dark}
+      >
+        <GlobalStyles />
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<App />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/recipes/:id" element={<Recipe />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/contribute" element={<Contribute />} />
+            <Route
+              path="/update-password/:token"
+              element={<UpdatePassword />}
+            />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
---
+++ src/styles/themes.js
@@ -0,0 +1,41 @@
+import { colors } from "../constants";
+
+export const light = {
+  // TODO - Add light theme
+};
+
+export const dark = {
+  error: colors.red,
+  success: colors.green,
+  hr: colors.gray,
+
+  background: {
+    main: colors.grayDark,
+    dark: colors.black,
+  },
+
+  border: {
+    main: colors.purpleCyan,
+    focus: colors.pinkPurple,
+  },
+
+  text: {
+    main: colors.white,
+    secondary: colors.blue,
+    link: colors.cyan,
+    hover: colors.pink,
+    button: colors.black,
+    placeholder: colors.blue,
+  },
+
+  heading: {
+    main: colors.yellowPink,
+    secondary: colors.blue,
+  },
+
+  button: {
+    main: colors.purpleCyan,
+    hover: colors.pinkPurple,
+    active: colors.pink,
+  },
+};
