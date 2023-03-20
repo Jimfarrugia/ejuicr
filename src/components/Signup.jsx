44c08ee786d3aa528fc38b55dd6a8d47b3700873
jimfarrugia <the.jim.farrugia@gmail.com>
2023-03-20 17:41:05 +1100

    display spinner during singup request

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Spinner from "./Spinner";
 import { API_URL } from "../constants";
 import { validateEmail, validatePassword } from "../helpers";
 
@@ -9,90 +10,97 @@ const Signup = ({ handleClickBack, handleClickLoginWithEmail }) => {
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [error, setError] = useState("");
-
-  const registerUser = async (userData) => {
-    const response = await axios.post(`${API_URL}/api/user`, userData);
-    if (response.data) {
-      localStorage.setItem("user", JSON.stringify(response.data));
-    }
-    return response.data;
-  };
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
+    setIsLoading(true);
     if (!validateEmail(email)) {
+      setIsLoading(false);
       return setError("Email is invalid.");
     }
     if (validatePassword(password) !== true) {
+      setIsLoading(false);
       return setError(validatePassword(password));
     }
     if (password !== passwordConfirm) {
+      setIsLoading(false);
       return setError("Passwords do not match.");
     }
-    try {
-      await registerUser({ email, password });
-    } catch (error) {
-      setError(
-        error.response.data.message || "Failed to register new account."
-      );
-      return console.log(error);
-    }
-    handleClickBack();
+    axios
+      .post(`${API_URL}/api/user`, { email, password })
+      .then((response) => {
+        localStorage.setItem("user", JSON.stringify(response.data));
+        setIsLoading(false);
+        handleClickBack();
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(
+          error.response.data.message || "Failed to register new account."
+        );
+        setIsLoading(false);
+      });
   };
 
   return (
     <>
       <h3>Signup</h3>
       {error && <div className="error-message">{error}</div>}
-      <form onSubmit={handleSubmit}>
-        <div className="sidemenu-form-row">
-          <div className="input-border">
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="sidemenu-form-row">
-          <div className="input-border">
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="sidemenu-form-row">
-          <div className="input-border">
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              value={passwordConfirm}
-              onChange={(e) => setPasswordConfirm(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="sidemenu-form-row">
-          <button type="button" className="red" onClick={handleClickBack}>
-            Cancel
-          </button>
-          <button type="submit" className="green">
-            Sign Up
-          </button>
-        </div>
-      </form>
-      <p className="small">
-        Already have an account?
-        <br />
-        <Link to="#" onClick={handleClickLoginWithEmail}>
-          Sign in instead
-        </Link>
-        .
-      </p>
+      {isLoading && <Spinner />}
+      {!isLoading && (
+        <>
+          <form onSubmit={handleSubmit}>
+            <div className="sidemenu-form-row">
+              <div className="input-border">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="sidemenu-form-row">
+              <div className="input-border">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="sidemenu-form-row">
+              <div className="input-border">
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={passwordConfirm}
+                  onChange={(e) => setPasswordConfirm(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="sidemenu-form-row">
+              <button type="button" className="red" onClick={handleClickBack}>
+                Cancel
+              </button>
+              <button type="submit" className="green">
+                Sign Up
+              </button>
+            </div>
+          </form>
+          <p className="small">
+            Already have an account?
+            <br />
+            <Link to="#" onClick={handleClickLoginWithEmail}>
+              Sign in instead
+            </Link>
+            .
+          </p>
+        </>
+      )}
     </>
   );
 };
