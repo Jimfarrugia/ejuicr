b2bad8f29c6c32f8b86e9af3b181532cd03809c3
jimfarrugia <the.jim.farrugia@gmail.com>
2023-03-20 17:39:53 +1100

    display spinner while recipe is being fetched

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -2,11 +2,13 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import App from "../App";
+import Spinner from "./Spinner";
 import { API_URL } from "../constants";
 
 const Recipe = () => {
   const { id } = useParams();
   const [recipe, setRecipe] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(false);
 
   const user = JSON.parse(localStorage.getItem("user"));
   const headers = {
@@ -14,15 +16,20 @@ const Recipe = () => {
   };
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(`${API_URL}/api/recipes/${id}`, { headers })
       .then((response) => {
         setRecipe(response.data);
+        setIsLoading(false);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false);
+      });
   }, []);
 
-  return <App recipe={recipe} />;
+  return <>{isLoading ? <Spinner /> : <App recipe={recipe} />}</>;
 };
 
 export default Recipe;
