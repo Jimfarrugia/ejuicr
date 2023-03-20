bc5f8880ab87d3075636501e6d2cae40c73b5061
jimfarrugia <the.jim.farrugia@gmail.com>
2023-03-20 17:39:29 +1100

    display spinner during login request

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Spinner from "./Spinner";
 import { API_URL } from "../constants";
 import { validateEmail } from "../helpers";
 
@@ -12,85 +13,91 @@ const Login = ({
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
-  const login = async (userData) => {
-    const response = await axios.post(`${API_URL}/api/user/login`, userData);
-    if (response.data) {
-      localStorage.setItem("user", JSON.stringify(response.data));
-    }
-    return response.data;
-  };
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
+    setIsLoading(true);
     if (!email || !password) {
+      setIsLoading(false);
       return setError("Please fill out all fields.");
     }
     if (!validateEmail(email)) {
+      setIsLoading(false);
       return setError("Email is invalid.");
     }
-    try {
-      await login({ email, password });
-    } catch (error) {
-      setError(
-        error.response.data.message || "Failed to register new account."
-      );
-      return console.log(error);
-    }
-    handleClickBack();
+    axios
+      .post(`${API_URL}/api/user/login`, { email, password })
+      .then((response) => {
+        localStorage.setItem("user", JSON.stringify(response.data));
+        setIsLoading(false);
+        handleClickBack();
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(
+          error.response.data.message || "Failed to register new account."
+        );
+        setIsLoading(false);
+      });
   };
 
   return (
     <>
       <h3>Login</h3>
       {error && <div className="error-message">{error}</div>}
-      <form onSubmit={handleSubmit}>
-        <div className="sidemenu-form-row">
-          <div className="input-border">
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="sidemenu-form-row">
-          <div className="input-border">
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="sidemenu-form-row">
-          <button type="button" className="red" onClick={handleClickBack}>
-            Cancel
-          </button>
-          <button type="submit" className="green">
-            Sign In
-          </button>
-        </div>
-      </form>
-      <p className="small">
-        Need an account?
-        <br />
-        <Link to="#" onClick={handleClickSignupWithEmail}>
-          Sign up instead
-        </Link>
-        .
-      </p>
-      <p className="small">
-        Forgot your password?
-        <br />
-        <Link to="#" onClick={handleClickResetPassword}>
-          Reset it here
-        </Link>
-        .
-      </p>
+      {isLoading && <Spinner />}
+      {!isLoading && (
+        <>
+          <form onSubmit={handleSubmit}>
+            <div className="sidemenu-form-row">
+              <div className="input-border">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="sidemenu-form-row">
+              <div className="input-border">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="sidemenu-form-row">
+              <button type="button" className="red" onClick={handleClickBack}>
+                Cancel
+              </button>
+              <button type="submit" className="green">
+                Sign In
+              </button>
+            </div>
+          </form>
+          <p className="small">
+            Need an account?
+            <br />
+            <Link to="#" onClick={handleClickSignupWithEmail}>
+              Sign up instead
+            </Link>
+            .
+          </p>
+          <p className="small">
+            Forgot your password?
+            <br />
+            <Link to="#" onClick={handleClickResetPassword}>
+              Reset it here
+            </Link>
+            .
+          </p>
+        </>
+      )}
     </>
   );
 };
