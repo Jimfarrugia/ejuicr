022f11e397141c0ff4cc6e063cb93859f04d1f9a
jimfarrugia <the.jim.farrugia@gmail.com>
2023-04-27 17:11:47 +1000

    refactor sidemenu UserProfile

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -3,43 +3,33 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGoogle, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const UserProfile = ({ user }) => {
-  const authProvider =
-    (user.authProvider && user.authProvider === "google" && "google") ||
-    (user.authProvider && user.authProvider === "twitter" && "twitter") ||
-    null;
-
-  const imgAltText =
-    authProvider === "twitter"
-      ? `${user.handle} ${authProvider} profile picture`
-      : `${user.displayName} ${authProvider} profile picture`;
+  const authProvider = user.authProvider || null;
+  const imgAltText = `${
+    user.handle || user.displayName
+  } ${authProvider} profile picture`;
 
   return (
     <div className="user-data">
       <p>You are signed in as:</p>
+      {user.picture && (
+        <div className="user-picture">
+          <img
+            src={user.picture}
+            alt={imgAltText}
+            referrerPolicy="no-referrer"
+          />
+        </div>
+      )}
       {(authProvider === "google" && (
-        <>
-          <div className="user-picture">
-            <img
-              src={user.picture}
-              alt={imgAltText}
-              referrerPolicy="no-referrer"
-            />
-          </div>
-          <p>
-            <FontAwesomeIcon icon={faGoogle} />
-            {user.displayName}
-          </p>
-        </>
+        <p>
+          <FontAwesomeIcon icon={faGoogle} />
+          {user.displayName}
+        </p>
       )) ||
         (authProvider === "twitter" && (
-          <>
-            <div className="user-picture">
-              <img src={user.picture} alt={imgAltText} />
-            </div>
-            <p>
-              <FontAwesomeIcon icon={faTwitter} />@{user.handle}
-            </p>
-          </>
+          <p>
+            <FontAwesomeIcon icon={faTwitter} />@{user.handle}
+          </p>
         )) || (
           <p>
             <FontAwesomeIcon icon={faEnvelope} />
