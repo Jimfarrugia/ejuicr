70170ba13294411aab636e573e47a7ca14b87d2f
jimfarrugia <the.jim.farrugia@gmail.com>
2023-04-27 12:18:02 +1000

    add helper to capitalize first letter in a string

--- src/helpers.js
+++ src/helpers.js
@@ -89,3 +89,8 @@ export const decodeToken = (encodedToken) => {
     return null;
   }
 };
+
+// Capitalize the first letter in a string
+export const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
